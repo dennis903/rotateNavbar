c9f56f0becb4661b05a73823b8a78f11904da224
hyeolee <hyeolee@zempot.com>
2023-04-07 17:50:28 +0900

    almost done

--- index.js
+++ index.js
@@ -1,46 +1,124 @@
-const setListPosition = () => {
-	const lists = $('#green-dot-lists').children();
-	const length = lists.length;
-	const rotate = Math.floor(360 / length);
-	const parentXPos = $('#green-dot-lists')[0].getBoundingClientRect().left;
-	const parentYPos = $('#green-dot-lists')[0].getBoundingClientRect().top;
+'use strict';
 
-	for (let i = 0; i < length; i++) {
+let isSetListPosition = false;
+
+$(function () {
+	const setListPosition = () => {
+		const lists = $('#green-dot-lists').children();
+		const length = lists.length;
+		const rotate = Math.floor(360 / length);
+		const parentXPos = $('#green-dot-lists')[0].getBoundingClientRect().left;
+		const parentYPos = $('#green-dot-lists')[0].getBoundingClientRect().top;
+
+		for (let i = 0; i < length; i++) {
 			const pos = {};
 			const list = $(lists[i]);
 
 			list.css('transform', `rotate(+${rotate * i}deg) translate(0, -50%)`);
-			list.children().eq(0).css('transform', `rotate(${-(rotate * i)}deg)`);
+			list.children().css('transform', `rotate(${-(rotate * i)}deg)`);
 
-			pos.x = list.children().eq(0)[0].getBoundingClientRect().left - parentXPos;
-			pos.y = list.children().eq(0)[0].getBoundingClientRect().top - parentYPos + 25;
+			pos.x = list.children()[0].getBoundingClientRect().left - parentXPos;
+			pos.y = list.children()[0].getBoundingClientRect().top - parentYPos + 25;
 
 			list.css('width', '110px');
 			list.css('height', '100px');
 			list.css('top', `${pos.y}px`);
 			list.css('left', `${pos.x}px`);
+		}
 	}
-}
 
-$(function () {
-	const greenDot = $('.green-dot-wrapper');
+	const isMobile = () => /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
-	$('#green-dot-button').on('click', (event) => {
-		const $this = $(event.currentTarget);
+	const rotateList = () => {
+		const isMobileDevice = isMobile();
+		const listsWrapper = $('#green-dot-lists');
+		const lists = listsWrapper.children();
 
-		if (greenDot.css('display') === 'none') {
-			greenDot.show();
-			$this.hide();
+		let offsetRadius = 0;
+		let targetRadius = 0;
+		let previousRadius = 0;
+
+		const touchStart = (event) => {
+			offsetRadius = getRadius(event);
+			previousRadius = offsetRadius;
+
+			if (isMobileDevice) {
+				listsWrapper.on('touchmove', touchMove);
+				listsWrapper.on('touchEnd', touchEnd);
+			} else {
+				listsWrapper.on('mousemove', touchMove);
+				listsWrapper.on('mouseup', touchEnd);
+			}
 		}
-	})
-
-	$('#green-dot-close-button').on('click', (event) => {
-		const $this = $(event.currentTarget);
-
-		if (greenDot.css('display') !== 'none') {
-			greenDot.hide();
-			$('#green-dot-button').show();
+	
+		if (isMobileDevice) {
+			listsWrapper.on('touchstart', touchStart);
+		} else {
+			listsWrapper.on('mousedown', touchStart);
 		}
+
+		const touchMove = (event) => {
+			const newRadius = getRadius(event);
+			targetRadius += (newRadius - previousRadius);
+			const rotate = targetRadius / Math.PI * 180;
+			previousRadius = newRadius;
+			listsWrapper.css('transform', `rotate(${rotate}deg)`);
+
+			for (let i = 0; i < lists.length; i++) {
+				$(lists[i]).css('transform', `rotate(${-rotate}deg)`);
+			}
+		}
+
+		const touchEnd = () => {
+			console.log('end');
+		}
+
+		const getRadius = (event) => {
+			const pos = mousePos(event, listsWrapper[0]);
+			const x = pos.x - listsWrapper[0].clientWidth * 0.5;
+			const y = pos.y - listsWrapper[0].clientHeight * 0.5;
+
+			return Math.atan2(y, x);
+		}
+
+		const mousePos = (event, currentElement) => {
+			let totalOffsetX = 0;
+			let totalOffsetY = 0;
+			let x = 0;
+			let y = 0;
+
+			for (; currentElement !== null; currentElement = currentElement.offsetParent) {
+				totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
+				totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
+			}
+
+			if (isMobileDevice) {
+					x = event.changedTouches[0].pageX - totalOffsetX;
+					y = event.changedTouches[0].pageY - totalOffsetY;
+			} else {
+					x = event.pageX - totalOffsetX;
+					y = event.pageY - totalOffsetY;
+			}
+
+			return {
+					x,
+					y
+			};
+		}
+	}
+
+	$('#green-dot-button').on('click', () => {
+		$('.green-dot-wrapper').fadeIn(250).show();
+		$('#green-dot-button').hide();
+		if (!isSetListPosition) {
+			setListPosition();
+			isSetListPosition = true;
+		}
+		rotateList();
 	})
-	setListPosition();
+	$('#green-dot-close-button').on('click', () => {
+		$('.green-dot-wrapper').fadeOut(250);
+		$('#green-dot-button').show();
+	})
 })
+
